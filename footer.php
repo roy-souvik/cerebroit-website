878e1fa38aba51f6e3bbcfd168bebb1b051e3a5f
Souvik Roy <souvik0789@gmail.com>
2018-11-28 17:25:00 +0530

    hide twitter link

--- footer.php
+++ footer.php
@@ -19,8 +19,8 @@
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.easing.min.js"></script>
-	<script src="js/jquery.scrollTo.js"></script>
-	<script src="js/wow.min.js"></script>
+		<script src="js/jquery.scrollTo.js"></script>
+		<script src="js/wow.min.js"></script>
     <!-- Custom Theme JavaScript -->
     <script src="js/custom.js"></script>
 	<!-- My Custom Theme JavaScript -->
